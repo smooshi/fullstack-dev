05802bc29a0dc1f645840cd73ff4c0404c825cea
smooshi <smooshi@users.noreply.github.com>
2018-01-14 18:32:21 +0200

    Viikko 1, Tehtävät: 1,2

--- osa/src/index.js
+++ osa/src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return (
+const Otsikko = (kurssi) => {
+    return(
+      <div>
+      <h1>Nimi: {kurssi.name}</h1>
+      </div>
+    )
+}
+
+const Osa = (osa) => {
+  return(
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>{a} plus {b} is {a + b}</p>
+    <p>{osa.name} {osa.tehtavat}</p>
     </div>
   )
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Sisalto = (osat) => {
+    return(
+      <div>
+      <Osa name= {osat.osa1} tehtavat= {osat.tehtavat1} />
+      <Osa name= {osat.osa2} tehtavat= {osat.tehtavat2} />
+      <Osa name= {osat.osa3} tehtavat= {osat.tehtavat3} />
+      </div>
+    )
+}
+
+const Yhteensa = (yht) => {
+  return(
+    <div>
+    <p>Yhteensä {yht.yht} tehtävää</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko name={kurssi} />
+      <Sisalto osa1={osa1} tehtavat1={tehtavia1} osa2={osa2} tehtavat2={tehtavia2} osa3={osa3} tehtavat3={tehtavia3}/>
+      <Yhteensa yht={tehtavia1 + tehtavia2 + tehtavia3} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
